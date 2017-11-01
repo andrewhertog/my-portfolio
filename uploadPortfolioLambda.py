a2ace51ecada1e1907b798f676702f75aee1c2cb
Andrew den Hertog <andrew.denhertog@prodigygame.com>
2017-10-31 22:03:44 -0400

    Lambda deploy function

---
+++ uploadPortfolioLambda.py
@@ -0,0 +1,19 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('www.denhertog.ca')
+artifact_bucket = s3.Bucket('denhertog.ca-codepipeline-artifacts')
+
+portfolio_zip = StringIO.StringIO()
+artifact_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
