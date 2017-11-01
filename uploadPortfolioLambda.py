63d03aa98544e443d622236f2e464a6a0f0e9033
Andrew den Hertog <andrew.denhertog@prodigygame.com>
2017-10-31 22:16:38 -0400

    update function

--- uploadPortfolioLambda.py
+++ uploadPortfolioLambda.py
@@ -3,17 +3,18 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('www.denhertog.ca')
-artifact_bucket = s3.Bucket('denhertog.ca-codepipeline-artifacts')
+    portfolio_bucket = s3.Bucket('www.denhertog.ca')
+    artifact_bucket = s3.Bucket('denhertog.ca-codepipeline-artifacts')
 
-portfolio_zip = StringIO.StringIO()
-artifact_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    artifact_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
